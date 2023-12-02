e6b3a4ee5d23c6cf4a45d2f9f3ac654542f01199
Rahul Masal <rahulmasal2013@gmail.com>
2023-12-02 23:04:50 +0530

    password hashing

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 //jshint esversion:6
 require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+// const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
 
 const app = express();
-console.log(process.env.API_KEY);
+// console.log(process.env.API_KEY);
 const secret = process.env.SECRET;
 
 app.use(express.static('public'));
@@ -23,34 +29,53 @@ const userSchema = new mongoose.Schema({
 
 
 
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
 const User = mongoose.model('User', userSchema);
 
 app.post('/register', async function (req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
+    bcrypt.hash(req.body.password, saltRounds, async function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        try {
+            const user = await newUser.save();
+            res.render('secrets');
+        } catch (error) {
+            console.log(error);
+        }
+
+        // const newUser = new User({
+        //     email: req.body.username,
+        //     password: md5(req.body.password)
+        // });
+
+        // try {
+        //     const user = await newUser.save();
+        //     res.render('secrets');
+        // } catch (error) {
+        //     console.log(error);
+        // }
+
     });
-
-    try {
-        const user = await newUser.save();
-        res.render('secrets');
-    } catch (error) {
-        console.log(error);
-    }
-
 });
 
 app.post('/login', async function (req, res) {
     const username = req.body.username;
+    // const password = md5(req.body.password);
     const password = req.body.password;
     try {
         const foundUser = await User.findOne({ email: username });
         if (foundUser) {
-            if (foundUser.password === password) {
-                res.render('secrets');
-            }
+            // if (foundUser.password === password) {
+            bcrypt.compare(password, foundUser.password, function (err, result) {
+                if (result === true) {
+                    res.render('secrets');
+                }
+            });
+
+
         }
     } catch (error) {
         console.log(error);
