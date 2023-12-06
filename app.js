858a5d278eb9d30663a528b0631a5e853f7c28d5
Rahul Masal <rahulmasal2013@gmail.com>
2023-12-06 12:04:39 +0530

    port added

--- app.js
+++ app.js
@@ -274,7 +274,10 @@ app.get('/register', function (req, res) {
 });
 
 
-
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Server started on port 3000');
 });
+
+// app.listen(3000, function () {
+//     console.log('Server started on port 3000');
+// });
